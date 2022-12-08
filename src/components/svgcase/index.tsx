71974f09e2d5b82fd1988a117973cdb9d92f9f38
Zenyet <zenyet1998@gmail.com>
2022-12-08 23:07:09 +0800

    rebuilt SvgInfo component.

--- src/components/svgcase/index.tsx
+++ src/components/svgcase/index.tsx
@@ -1,21 +1,33 @@
 import Copy from "../button/Copy";
 import Download from "../button/Download";
-import {SVGInfo} from "../../types/SVGInfo";
 import useWillChange from "../../hooks/useWillChange";
+import useDynamicImport from "../../hooks/useDynamicImport";
 import './style.scss';
+import React, {useRef} from "react";
+
+interface SVGInfo{
+    filename: string,
+}
 
 
 function SVGCase(props: SVGInfo) {
     const {filename} = props;
     const style = useWillChange();
+    const [loading, svgHolder] = useDynamicImport(filename);
+    const refer = useRef<null | HTMLElement>(null)
+
 
     return (
         <div className='svgCon'>
-            <img className='svgImage' src={`/svgs/${filename}.svg`} alt={filename}
-                style={style}
-            />
+            {/*<img className='svgImage' src={`/svgs/${filename}.svg`} alt={filename}*/}
+            {/*    style={style}*/}
+            {/*/>*/}
+            <i className='svg-wrapper' style={style} ref={refer}>
+                {loading ? <div>loading</div> :
+                    svgHolder}
+            </i>
             <footer className='footer'>
-                <Copy filename={filename}/>
+                <Copy refer={refer}/>
                 <Download filename={filename}/>
             </footer>
         </div>
